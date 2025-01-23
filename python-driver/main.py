67d00dd846b8013605943f742daf937238627091
looeejee <luigi.brienza@neo4j.com>
2025-01-23 11:25:04 +0100

    adding files to project

---
+++ python-driver/main.py
@@ -0,0 +1,14 @@
+import dotenv
+import os
+from neo4j import GraphDatabase
+
+load_status = dotenv.load_dotenv(".env")
+if load_status is False:
+    raise RuntimeError('Environment variables not loaded.')
+
+URI = os.getenv("NEO4J_URI")
+AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    print("Connection established.")
